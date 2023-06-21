8741dc606fea35a20890b7e3f71a39be50de01e7
root <root@kali.kali>
2023-06-21 19:22:15 -0400

    Added funtion send message to user

--- main.js
+++ main.js
@@ -1,13 +1,28 @@
 const Insta = require('./insta.js');
 const client = new Insta.Client();
 
+function sendMessageToUser(chaID, message, type){
+    client.fetchChat(chaID).then((chat) => {
+        try{
+            if(type=='image'){
+                chat.sendPhoto(message);
+                try {
+                    fs.rmdirSync('./files/'+chaID, { recursive: true });
+                }catch (err) {}
+            }else{
+                chat.sendMessage(message);
+            }
+        }catch(err){}
+    });
+}
+
 client.on('connected', () => {
     console.log('I am ready!')
 })
 
 
 client.on('messageCreate', message => {
-    client.sendMessage(message)
+    sendMessageToUser(message.chatID, message)
 })
 
 client.login('zizifa_2001', 'mumu1379');
