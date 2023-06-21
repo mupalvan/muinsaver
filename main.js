45fcf75c054cfdcea0e5fb59543a4eab784141b9
root <root@kali.kali>
2023-06-21 19:16:48 -0400

    CompliteL Login and check message

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ client.on('connected', () => {
 
 
 client.on('messageCreate', message => {
-    message.createMessageCollector(message)
+    message.sendMessage(message)
 })
 
 client.login('zizifa_2001', 'mumu1379');
