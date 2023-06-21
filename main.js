8f38bdeccadb79c52ba22e1f0d16d83da54771dc
root <root@kali.kali>
2023-06-21 19:01:35 -0400

    change login code

--- main.js
+++ main.js
@@ -1,19 +1,14 @@
 const Insta = require('./insta.js');
 const client = new Insta.Client();
 
-client.on('connected', async () => {
-    console.log(`${client.user.username} Is Ready Now For Chats`);
-    client.on('pendingRequest', async data => {
-      try{
-        client.on('messageCreate', msg => {
-            if (msg.content === 'ping') {
-              msg.reply('Pong!');
-            }
-          });
-      }catch(err){}
-    });
-});
-
-
+client.on('connect', () => {
+    console.log(`Logged in as ${client.user.username}!`);
+  });
+  
+  client.on('messageCreate', msg => {
+    if (msg.content === 'ping') {
+      msg.reply('Pong!');
+    }
+  });
 
 client.login('zizifa_2001', 'mumu1379');
